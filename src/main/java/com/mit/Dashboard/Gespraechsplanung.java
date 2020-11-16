ee4502a273f186166a5205eb1abae340dc269036
Fabian Kirschbaum <fabian.kirschbaum@alumni.fh-aachen.de>
2020-11-16 21:21:21 +0100

    initial commit

---
+++ src/main/java/com/mit/Dashboard/Gespraechsplanung.java
@@ -0,0 +1,127 @@
+package com.mit.Dashboard;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "gespraechsplanung")
+public class Gespraechsplanung {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "KUNDENNUMMER")
+    private int kundennummer;
+
+    @Column(name = "KATEGORIE")
+    private String kategorie;
+
+    @Column(name = "THEMA")
+    private String thema;
+
+    @Column(name = "DATUM")
+    private String datum;
+
+    @Column(name = "ZEITRAUM")
+    private String zeitraum;
+
+    @Column(name = "BERATER")
+    private String berater;
+
+    @Column(name = "NAECHSTEFAELLIGKEIT")
+    private String  naechsteFaelligkeit;
+
+    @Column(name = "LETZTERKONTAKT")
+    private String  letzterKontakt;
+
+    //Datum, Zeitraum, Berater, nächsteFälligkeit, letzterkontakt
+    public Gespraechsplanung() {
+    }
+
+    /*public Gespraechsplanung(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }*/
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getKundennummer() {
+        return kundennummer;
+    }
+
+    public void setKundennummer(int kundennummer) {
+        this.kundennummer = kundennummer;
+    }
+
+    public String getKategorie() {
+        return kategorie;
+    }
+
+    public void setKategorie(String kategorie) {
+        this.kategorie = kategorie;
+    }
+
+    public String getDatum() {
+        return datum;
+    }
+
+    public void setDatum(String datum) {
+        this.datum = datum;
+    }
+
+    public String getZeitraum() {
+        return zeitraum;
+    }
+
+    public void setZeitraum(String zeitraum) {
+        this.zeitraum = zeitraum;
+    }
+
+    public String getBerater() {
+        return berater;
+    }
+
+    public void setBerater(String berater) {
+        this.berater = berater;
+    }
+
+    public String getNaechsteFaelligkeit() {
+        return naechsteFaelligkeit;
+    }
+
+    public void setNaechsteFaelligkeit(String naechsteFaelligkeit) {
+        this.naechsteFaelligkeit = naechsteFaelligkeit;
+    }
+
+    public String getLetzterKontakt() {
+        return letzterKontakt;
+    }
+
+    public void setLetzterKontakt(String letzterKontakt) {
+        this.letzterKontakt = letzterKontakt;
+    }
+
+    public String getThema() {
+        return thema;
+    }
+
+    public void setThema(String thema) {
+        this.thema = thema;
+    }
+
+    @Override
+    public String toString() {
+        return "Gespraech [id=" + id + ", Kundennummer=" + kundennummer + ", Thema=" + thema + ", Nächste Fälligkeit=" + naechsteFaelligkeit + "]";
+    }
+}
---
+++ src/main/java/com/mit/Dashboard/GespraechsplanungRepository.java
@@ -0,0 +1,10 @@
+package com.mit.Dashboard;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import com.mit.Dashboard.Gespraechsplanung;
+
+public interface GespraechsplanungRepository extends CrudRepository<Gespraechsplanung, Long> {
+    List<Gespraechsplanung> findByKundennummer(int kundennummer);
+}
